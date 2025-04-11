0aed7db9d260eca70d3adadcc7548892d8549a71
jelyananandah <jelyanananda@gmail.com>
2025-04-11 10:02:10 +0800

    Add files via upload

---
+++ meett1.py
@@ -0,0 +1,34 @@
+# perbandingan kemungkinan nilai = true/false
+# <, >, >=, <=, !=, not
+# a = 2 nim awal
+# b = 2 nim akhir
+
+a = 24
+b = 75
+hasil = a < b
+print(hasil)
+
+a = 24
+b = 75
+hasil = a > b
+print(hasil)
+
+a = 24
+b = 75
+hasil = a <= b
+print(hasil)
+
+a = 24
+b = 75
+hasil = a >= b
+print(hasil)
+
+a = 24
+b = 75
+hasil = a != b
+print(hasil)
+
+a = 24
+b = 75
+("hasil = a not b")
+print(hasil)
