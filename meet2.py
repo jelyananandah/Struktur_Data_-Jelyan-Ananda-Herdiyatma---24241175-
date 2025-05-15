e5ae3c0d414dc69019cf4cdd5d5cb2cf5cc63869
jelyananandah <jelyanananda@gmail.com>
2025-05-15 22:18:46 +0800

    Add files via upload

---
+++ meet2.py
@@ -0,0 +1,27 @@
+# operasi Logika
+# or and xor not
+
+x = True
+z = not x
+print("nilai dari z =", z)
+
+print("********** and ***********")
+x = True
+y = True
+z = x and y
+print(x, 'and', y, '=', z)
+
+x = True
+y = False
+z = x and y
+print(x, 'and', y, '=', z)
+
+x = False
+y = True
+z = x and y
+print(x, 'and', y, '=', z)
+
+x = False
+y = False
+z = x and y
+print(x, 'and',y,'=',z)
