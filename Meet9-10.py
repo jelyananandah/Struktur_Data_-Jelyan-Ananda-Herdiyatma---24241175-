be1de21dbaeed3aae2f2de6de279de46c988a3b6
jelyananandah <jelyanananda@gmail.com>
2025-07-14 05:38:13 -0700

    Create Meet9-10.py

---
+++ Meet9-10.py
@@ -0,0 +1,7 @@
++ meminta input
++ domain = input("Masukkan nama domain anda: ")
++ #memsihkkan antara titik
++ nama, ekstensi = domain.split(".")
++ # Menampilkan 
++ print("Nama domain anda adalah =", nama)
++ print("Nama yang anda gunakan adalah = ", ekstensi)
