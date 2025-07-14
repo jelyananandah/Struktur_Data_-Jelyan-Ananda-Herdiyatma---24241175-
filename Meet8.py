24d433dc93d6b2189ff32063584fd809510a2a62
jelyananandah <jelyanananda@gmail.com>
2025-07-14 05:36:36 -0700

    Create Meet8.py

---
+++ Meet8.py
@@ -0,0 +1,10 @@
++ # Variabel utama
++ number = "1234567890"
++ data_terakhir = number[-1]
++ data_pertama = number[0]
++ data_k3_sampai_k6 = number[2:6]
++
++ # Output hasil
++ print("a. Data terakhir:", data_terakhir)
++ print("b. Data pertama:", data_pertama)
++ print("c. Data ke-3 sampai ke-6:", data_k3_sampai_k6)
